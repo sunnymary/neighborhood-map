105a30337151242ebfe2560e86cf0f14fa59797b
sunnymary <mary.zhou.coding@gmail.com>
2017-07-12 12:03:13 -0500

    reorg the folder

---
+++ js/map.js
@@ -0,0 +1,16 @@
+
+// function to initialize the map
+function initMap() {
+  //store the center point in a variable
+  var fortWorth = {lat: 32.7554883, lng: -97.3307658};
+  //use a constructor to create a new map JS object.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: fortWorth
+  });
+
+  // var marker = new google.maps.Marker({
+  //   position: fortWorth,
+  //   map: map
+  // });
+}
