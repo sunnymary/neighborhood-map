09a5a936b105d3891588e18923fbfa595dfd73e2
sunnymary <mary.zhou.coding@gmail.com>
2017-07-18 17:31:03 -0500

    interaction between list and marker

--- js/app.js
+++ js/app.js
@@ -4,23 +4,26 @@ var Company = function(data){
     this.name = data.name;
     this.location = data.location;
     this.employment = data.employment;
-    this.headquaterAddress = data.headquaterAddress;
+    this.headquarterAddress = data.headquarterAddress;
+    this.lat = data.lat;
+    this.lng = data.lng;
 };
 
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-    this.company1 = ko.observable(companyList[0].name);
-
     var self = this;
     this.companyArray = ko.observableArray([]);
     companyList.forEach(function(oneCompany){
         self.companyArray.push(new Company(oneCompany));
     });
 
-    this.openInfowindowByClickListName = function(){
-
+    //click to show data
+    this.enableInfowindow = function(company){
+        new google.maps.event.trigger( company.marker, 'mouseover' );
     }
-
+    this.disableInfowindow = function(company){
+        new google.maps.event.trigger( company.marker, 'mouseout' );
+    }
 
     // this.firstName = ko.observable("Bert");
     // this.lastName = ko.observable("Bertington");
@@ -33,6 +36,8 @@ function AppViewModel() {
     // };
 }
 
+var appViewModel = new AppViewModel();
+
 // Activates knockout.js
-ko.applyBindings(new AppViewModel());
+ko.applyBindings(appViewModel);
 
--- js/map.js
+++ js/map.js
@@ -9,6 +9,41 @@ function initMap() {
     center: fortWorth
   });
 
+  // //loop through companyList to create markers and store in markers variable
+  // var markers = companyList.map(function(company){
+  //   return new google.maps.Marker({
+  //     position: company,
+  //     map: map
+  //   });
+  // });
+
+  // var infowindows = companyList.map(function(company){
+  //   var contentString =
+  //     '<h4>'+ company.name +'</h4>'+
+  //     '<p>employment:' + company.employment + '</p>';
+
+  //   return new google.maps.InfoWindow({
+  //     content: contentString
+  //   });
+  // })
+
+  // // markers[0].addListener('click', function() {
+  // //     infowindows[0].open(map, markers[0]);
+  // // });
+
+  // //TODO: problem with event CLOSURE
+  // for (var i = 0; i < markers.length; i++) {
+  //   markers[i].addListener('click', function() {
+  //     infowindows[i].open(map, markers[i]);
+  //   }(i));
+  // }
+
+
+
+  this.markers = [];
+  this.infowindows = [];
+
+
   //loop through companyList to create marker&infowindow
   companyList.forEach(function(company){
     var contentString =
@@ -24,6 +59,10 @@ function initMap() {
       map: map
     });
 
+    //store created marker/infowindow into list
+    markers.push(marker);
+    infowindows.push(infowindow);
+
     //add method to open/close infowindow
     marker.addListener('mouseover', function() {
       infowindow.open(map, marker);
@@ -33,4 +72,11 @@ function initMap() {
     });
   });
 
+  //add markers/infowindows to companyArray
+  //idea comes from Udacity forums Karol's comment - https://discussions.udacity.com/t/infowindow-not-opening-when-clicking-on-list-item-markers-name/305720/2
+  for (var i = 0; i < markers.length; i++) {
+    appViewModel.companyArray()[i].marker = markers[i];
+  }
+
 }
+
